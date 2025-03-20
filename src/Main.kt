a4b0ddbebd6251496f66d2cc762678c272e16c23
zeeshantufail <zeeshan.tufail@aboitiz.com>
2025-03-20 16:38:54 +0800

    lacho study

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,25 @@ fun main() {
     println("Enter snake length:")
     val length = takeInt()
 
-
-
+    if (length == 1) {
+        println("*")
+    } else if (length == 2) {
+        println("**")
+    } else if (length == 3) {
+        println("***")
+    } else if (length == 4) {
+        println("****")
+    } else if (length == 5) {
+        println("*****")
+    } else if (length == 6) {
+        println("******")
+    } else if (length == 7) {
+        println("*******")
+    } else if (length == 8) {
+        println("********")
+    } else if (length == 9) {
+        println("*********")
+    } else if (length == 10) {
+        println("**********")
+    }
 }
