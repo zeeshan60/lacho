238d475d5df195411ab5648d68eef94d27469c03
zeeshantufail <zeeshan.tufail@aboitiz.com>
2025-03-24 12:37:33 +0800

    lacho working

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,16 @@
 fun main() {
     println("Enter snake length:")
-    val length = takeInt()
 
-    if (length == 1) {
-        println("*")
-    } else if (length == 2) {
-        println("**")
-    } else if (length == 3) {
-        println("***")
-    } else if (length == 4) {
-        println("****")
-    } else if (length == 5) {
-        println("*****")
-    } else if (length == 6) {
-        println("******")
-    } else if (length == 7) {
-        println("*******")
-    } else if (length == 8) {
-        println("********")
-    } else if (length == 9) {
-        println("*********")
-    } else if (length == 10) {
-        println("**********")
-    }
+    val lenght=takeInt()
+//    for (i in 0 until lenght) {
+//        print("*")
+//    }
+        println("print snake box")
+        println("....................")
+        println("....................")
+        println("....................")
+        println("....................")
+        println("....................")
+
+
 }
