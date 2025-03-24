f1b1e8f3f526a73f869c88907402e51ae40d2f5f
zeeshantufail <zeeshan.tufail@aboitiz.com>
2025-03-24 13:22:12 +0800

    lacho working

--- src/Main.kt
+++ src/Main.kt
@@ -2,42 +2,42 @@ fun main() {
     println("Enter snake length:")
 
     println("print snake board ")
-    println("***.........")
-    println("............")
-    println("............")
-    println("............")
+    println("***.....")
+    println("........")
+    println("........")
+    println("........")
 
     println("Press g to move forward")
     val key = takeString()
     if (key == "g") {
 
-        println(".***........")
-        println("............")
-        println("............")
-        println("............")
+        println(".***....")
+        println("........")
+        println("........")
+        println("........")
     }
     println("Press g to move forward")
 val key1=takeString()
 if (key1=="g"){
-    println("..***.......")
-    println("............")
-    println("............")
-    println("............")
+    println("..***...")
+    println("........")
+    println("........")
+    println("........")
 }
     println("Press g to move forward")
     val key2=takeString()
     if (key2=="g"){
-        println("...***......")
-        println("...........")
-        println("...........")
-        println("...........")
+        println("...***.")
+        println(".......")
+        println(".......")
+        println(".......")
     }
     println("Press g to move forward")
     val key3=takeString()
     if (key3=="g"){
-        println("....****....")
-        println("............")
-        println("............")
-        println("............")
+        println("....****")
+        println("........")
+        println("........")
+        println("........")
     }
 }
