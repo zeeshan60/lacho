6fa8bc3c484c3944055aa7348dc6a86a52b9fef1
zeeshantufail <zeeshan.tufail@aboitiz.com>
2025-03-24 16:35:25 +0800

    lacho working

--- src/Main.kt
+++ src/Main.kt
@@ -7,40 +7,6 @@ fun main() {
     println("........")
     println("........")
 
-    println("Press g to move forward")
-    val key = takeString()
-    if (key == "g") {
-
-        println(".***....")
-        println("........")
-        println("........")
-        println("........")
-    }
-    println("Press g to move forward")
-    val key1 = takeString()
-    if (key1 == "g") {
-        println("..***...")
-        println("........")
-        println("........")
-        println("........")
-    }
-    println("Press g to move forward")
-    val key2 = takeString()
-    if (key2 == "g") {
-        println("...***.")
-        println(".......")
-        println(".......")
-        println(".......")
-    }
-    println("Press g to move forward")
-    val key3 = takeString()
-    if (key3 == "g") {
-        println(".....***")
-        println("........")
-        println("........")
-        println("........")
-    }
-
     for (i in 0 until 5) {
         println("Press g to move forward")
         val key = takeString()
