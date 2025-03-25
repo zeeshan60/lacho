645ddf016baf893b711a09450d0439bfe7923a6a
zeeshantufail <zeeshan.tufail@aboitiz.com>
2025-03-25 14:27:15 +0800

    lacho working

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,29 @@
 fun main() {
-    println("Enter snake length:")
 
     println("print snake board ")
     println("***.....")
     println("........")
     println("........")
     println("........")
-
+    var position = 0
     for (i in 0 until 5) {
-        println("Press g to move forward")
-        val key = takeString()
-        if (key == "g") {
 
-            println("***.....")
-            println("........")
-            println("........")
-            println("........")
+            println("Press g to move forward")
+            val key = takeString()
+            if (key == "g") {
+
+                print(".")
+                print("*")
+                print("*")
+                print("*")
+                print(".")
+                print(".")
+                print(".")
+                print(".")
+                println()
+                println("........")
+                println("........")
+                println("........")
+            }
         }
     }
-}
